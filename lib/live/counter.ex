fb875e93dcb6823c6f6c8b347414bc993f3322c5
hooneun <kimhoon0316@gmail.com>
2023-09-16 20:52:42 +0900

    clients state shared

--- lib/live/counter.ex
+++ lib/live/counter.ex
@@ -1,16 +1,28 @@
 defmodule CounterWeb.Counter do
   use CounterWeb, :live_view
 
+  # The topic is used to identify the LiveView process
+  @topic "live"
+
   def mount(_params, _session, socket) do
+    CounterWeb.Endpoint.subscribe(@topic)
     {:ok, assign(socket, :val, 0)}
   end
 
   def handle_event("inc", _, socket) do
-    {:noreply, update(socket, :val, &(&1 + 1))}
+    new_state = update(socket, :val, &(&1 + 1))
+    CounterWeb.Endpoint.broadcast_from(self(), @topic, "inc", new_state.assigns)
+    {:noreply, new_state}
   end
 
   def handle_event("dec", _, socket) do
+    new_state = update(socket, :val, &(&1 - 1))
+    CounterWeb.Endpoint.broadcast_from(self(), @topic, "dec", new_state.assigns)
     {:noreply, update(socket, :val, &(&1 - 1))}
+  end
+
+  def handle_info(msg, socket) do
+    {:noreply, assign(socket, val: msg.payload.val)}
   end
 
   def render(assigns) do
